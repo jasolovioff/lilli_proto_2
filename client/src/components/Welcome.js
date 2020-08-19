d245efc3d95dcb967e3d1d18e9b807935efd034e
jorge <jorge@imatec.cl>
2020-08-18 21:56:11 -0400

    Header implementation.

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,5 +1,11 @@
 import React, {Component} from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import NavbarBrand from "react-bootstrap/NavbarBrand";
+
+import logoLili from "../static/images/lilichile.png";
 
 class Welcome extends Component{
     render() {
@@ -8,6 +14,22 @@ class Welcome extends Component{
         }
         return(
             <section>
+                <header className="container-fluid bg-white fixed-top pt-3">
+                    <Container>
+                        <Row>
+                            <Col xs={6}>
+                                <NavbarBrand href="/">
+                                    <img src={logoLili} alt="Logo Lili"/>
+                                </NavbarBrand>
+                            </Col>
+                        </Row>
+                    </Container>
+                </header>
+                <Container fluid as="section">
+                    <Container>
+
+                    </Container>
+                </Container>
                 <Jumbotron>
                     <h2>Tu salud, simple, conmigo</h2>
                     <p>
