86910b6ce59d5240769343fda1fb13edcc22f24f
jorge <jorge@imatec.cl>
2020-09-02 13:38:30 -0400

    Step 2 aspect updated.

--- client/src/components/Step2.js
+++ client/src/components/Step2.js
@@ -85,7 +85,7 @@ class Step2 extends Component {
                 <Container fluid as="section">
                     <Container>
                         <Row className="justify-content-md-center">
-                            <Col xs={12} lg={6} className="rounded shadow-lg bg-white overflow-hidden p-4 mb-5">
+                            <Col xs={12} lg={7} className="rounded shadow-lg bg-white overflow-hidden p-4 mb-5">
 
                                 <Row className="progress position-relative"
                                      style={{height: "6px", top: "-25px", margin: "0 -26px"}}>
@@ -97,9 +97,9 @@ class Step2 extends Component {
                                 <span className="h6 text-black-50 m-0">Paso {this.props.currentStep}</span>
                                 <h2 className="title mb-5">Datos personales</h2>
 
-                                <Form.Group className="mb-5 pb-3">
-                                    <Form.Label className="text-col1 h4">¿Cuál es tu edad?</Form.Label>
-                                    <div className="label-hv-animate">
+                                <Form.Group className="mb-5 row">
+                                    <Form.Label className="col col-9 h4">¿Cuál es tu edad?</Form.Label>
+                                    <div className="col-3">
                                         <Form.Control type="text" name="age"
                                                        className="form-control rounded-0 text-center control-animate border-0 bg-light"
                                                        aria-describedby="edad" maxLength="2" onChange={this.props.handleChange} value={this.props.age}/>
@@ -107,35 +107,37 @@ class Step2 extends Component {
                                 </Form.Group>
 
 
-                                <div className="mb-5 pb-3">
-                                    <Form.Group>
-                                        <Form.Label className="text-col1 h4">¿Tienes cargas? <br/>
-                                            <span className="text-black-50 font-weight-light"> Si las tienes, ingrésalas con sus edades</span>
-                                        </Form.Label>
-                                        <div className="float-right custom-control custom-switch">
-                                            <Form.Control type="checkbox" className="custom-control-input" id="tengocargas"
-                                                          data-toggle="collapse" data-target="#sumarcargas" name="tengoCargas"
-                                                          onChange={this.props.handleChange} aria-controls="example-collapse-text"
-                                                          aria-expanded={this.props.tengoCargas} checked={this.props.tengoCargas} />
-                                            <label className="custom-control-label" htmlFor="tengocargas"></label>
+                                    <Form.Group className="row">
+                                        <Form.Label className="col-9 mb-0 col">¿Tienes cargas?</Form.Label>
+                                        <div className="col-3 mb-0">
+                                            <div className="float-right custom-control custom-switch">
+                                                <Form.Control type="checkbox" className="custom-control-input" id="tengocargas"
+                                                              data-toggle="collapse" data-target="#sumarcargas" name="tengoCargas"
+                                                              onChange={this.props.handleChange} aria-controls="example-collapse-text"
+                                                              aria-expanded={this.props.tengoCargas} checked={this.props.tengoCargas} />
+                                                <label className="custom-control-label" htmlFor="tengocargas"></label>
+                                            </div>
+                                        </div>
+                                        <div className="col-12"><span className="text-black-50 font-weight-light"> Si las tienes, ingrésalas con sus edades</span>
                                         </div>
                                     </Form.Group>
+
                                     <Collapse in={this.props.tengoCargas}>
                                         <div id="example-collapse-text">
-                                            <Form.Group className="row">
+                                            <Form.Group className="row justify-content-end">
                                                 <Col xs={12}>
-                                                    <a className="float-right btn text-col5 text-hv-col5 h6 text-hv-col pr-0"
+                                                    <a className="float-right btn text-col1 text-hv-col1 h6 pr-0"
                                                        data-toggle="collapse" href="#carga2" role="button"
                                                        aria-expanded="false" aria-controls="collapseExample" onClick={this.appendCarga}>
-                                                        + Añadir nuevas cargas
+                                                        <i class="ic ic-user"></i> Incluir cargas
                                                     </a>
                                                 </Col>
                                             </Form.Group>
 
                                             {this.state.cargas.map((input,i)=>
-                                                <Form.Group className="row" key={"fgroup"+i.toString()}>
-                                                    <Form.Label className="col-2 col-form-label h5 text-muted pt-3">Carga {i+1}</Form.Label>
-                                                    <div className="col-5 input-group">
+                                                <Form.Group className="row justify-content-center" key={"fgroup"+i.toString()}>
+                                                    <Form.Label className="col-4 col-md-2 col-form-label h5 text-muted text-right pt-3">Carga {i+1}</Form.Label>
+                                                    <div className="col-6 col-md-5 input-group">
                                                         <Form.Control type="text" className="form-control text-center rounded-0 border-0 bg-light"
                                                                       maxLength="2" style={{maxWidth: "100px"}} name={"carga-"+(i).toString()}
                                                                       onChange={this.handleCargaInput}
@@ -149,40 +151,13 @@ class Step2 extends Component {
                                                     </div>
                                                 </Form.Group>
                                             )}
-
-
-                                            <div className="collapse" id="carga2">
-                                                <div className="form-group row mb-5">
-                                                    <label className="col-2 col-form-label h5 text-muted pt-3">Carga 2</label>
-                                                    <div className="col-5 input-group">
-                                                        <input type="text"
-                                                               className="form-control text-center rounded-0 border-0 bg-light"
-                                                               maxLength="2" placeholder="0" style={{maxWidth: "100px"}}/>
-                                                            <div className="input-group-prepend">
-                                                                <div
-                                                                    className="input-group-text  bg-white border-0">Años
-                                                                </div>
-                                                                <div className="input-group-append">
-                                                                    <button className="btn rounded-0 border-0 text-col1"
-                                                                            type="button" data-toggle="tooltip"
-                                                                            data-placement="top" title="Eliminar carga" >
-                                                                        <i className="ic ic-trash"></i></button>
-                                                                </div>
-                                                            </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-
                                             <div className="form-group row">
-                                                <div className="col-12">
-                                                    <small className="text-secondary font-italic float-left">*Si tu
-                                                        carga es menor a "1 año", indica “0 años”.</small>
+                                                <div className="col-12 text-center">
+                                                    <small className="text-black-50 font-italic float-left">*Si tu carga es menor a "1 año", indica “0 años”.</small>
                                                 </div>
                                             </div>
                                         </div>
                                     </Collapse>
-
-                                </div>
 
                                 <div className="form-group mt-5">
                                     <button className="btn bg-col1 text-white d-block mx-auto float-md-right my-2 py-3 px-4 shadow-lg mr-1 btn-bubble" onClick={this.props._next}>Siguiente</button>
