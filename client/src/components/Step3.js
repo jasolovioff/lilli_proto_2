75760e19ecb8abf831ad2f6a073fae1e23e336d1
jorge <jorge@imatec.cl>
2020-09-03 01:20:14 -0400

    Added plan and deductible matrix.

--- client/src/components/Step3.js
+++ client/src/components/Step3.js
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import redes from "../utils/redes";
 
 class Step3 extends Component{
     render() {
@@ -14,117 +15,84 @@ class Step3 extends Component{
                 <Container as="section" fluid>
                     <Container>
                         <Row className="justify-content-md-center">
-                            <Col xs={12} lg={6} className="rounded shadow-lg bg-white overflow-hidden p-4 mb-5">
+                            <Col xs={12} lg={7} className="rounded shadow-lg bg-white overflow-hidden p-4 mb-5">
 
-                                <Row className="progress position-relative"
+                                <Row className="progress row position-relative"
                                      style={{height: "6px", top: "-25px", margin: "0 -26px"}}>
                                     <div className="progress-bar stretchRight bg-col6" role="progressbar"
                                          style={{width: "75%"}} aria-valuenow="75" aria-valuemin="0"
                                          aria-valuemax="100"></div>
                                 </Row>
 
-                                <span className="h6 text-black-50 m-0">Paso {this.props.currentStep}</span>
+                                <span className="h6 text-black-50 m-0">Paso 3</span>
                                 <h2 className="title mb-5">Elige tus preferencias </h2>
 
-                                <Form.Group>
-                                    <h4 className="font-weight-bold text-col1">Elige tu Red</h4>
-                                    <h5 className="text-secondary">Grupo de clínicas en las que se harán efectivas las
-                                        coberturas de tu plan.<br/> La elección de la Red determina parte del precio que
-                                            pagarás mensualmente.</h5>
-                                </Form.Group>
-                                <Form.Group className="mb-3 pb-4">
-                                    <div className="custom-control custom-radio sd-option-list mb-3">
-                                        <Form.Control type="radio" id="red1" name="eligered" className="custom-control-input" value="red1" onChange={this.props.handleChange}/>
-                                            <Form.Label className="custom-control-label text-col1 font-weight-bold h4" htmlFor="red1">
-                                                RED 1
-                                                <ul className="row h5 text-black-50 font-weight-lighter list-inputradio">
-                                                    <li>Red Salud Stgo. y Providencia</li>
-                                                    <li>Clínica Dávila</li>
-                                                    <li>Clínica Vespucio</li>
+                                <div className="form-group">
+                                    <h5 className="text-black-50">
+                                        A continuación, te invitamos a elegir tus preferencias haciendo click en la
+                                        combinación de tu interés en la siguiente tabla.
+                                    </h5>
+                                </div>
+
+
+                                <div className="d-block px-3 btn-group-toggle">
+                                    <Row className="row line-redydedusible-head">
+                                        <Col xs={3}></Col>
+                                        <Col xs={3} className="text-center text-col1 h6">Deducible UF 10</Col>
+                                        <Col xs={3} className="text-center text-col1 h6">Deducible UF 15</Col>
+                                        <Col xs={3} className="text-center text-col1 h6">Deducible UF 20</Col>
+                                    </Row>
+
+                                    {redes.map((input, i)=>
+                                        <Row className="line-redydedusible">
+                                            <Col xs={3} className="position-relative">
+                                                <span className="red-title h3 text-col1">{input.name}</span>
+                                                <ul className="d-block text-black-50 font-weight-lighter h6 list-red"
+                                                    style={{left: "30px", height: "auto"}}>
+                                                    {input.clinics.map((clinica, j)=>
+                                                        <li>{clinica}</li>
+                                                    )}
                                                 </ul>
+                                            </Col>
+                                            <Form.Label className="btn col-3 text-center position-relative btn-secondary" role="button"
+                                                        data-twbs-toggle-buttons-class-active="redydedusible"
+                                                        aria-pressed="false" data-original-title="">
+                                                <Form.Control type="radio" name="options" value="1" />
+                                                <div className="position-absolute center-absolute">
+                                                    <span className="font-weight-bold h4">UF 1,61</span>
+                                                    <small className="d-block text-black-50 font-weight-lighter">$46.172</small>
+                                                </div>
                                             </Form.Label>
-                                    </div>
+                                            <Form.Label className="btn col-3 btn-secondary" role="button"
+                                                        data-twbs-toggle-buttons-class-active="redydedusible"
+                                                        aria-pressed="false" data-original-title="">
+                                                <Form.Control type="radio" name="options" value="2" />
+                                                <div className="position-absolute center-absolute">
+                                                    <span className="font-weight-bold h4">UF 1,52</span>
+                                                    <small className="d-block text-black-50 font-weight-lighter">$43.591</small>
+                                                </div>
+                                            </Form.Label>
+                                            <Form.Label className="btn col-3 btn-secondary" role="button"
+                                                        data-twbs-toggle-buttons-class-active="redydedusible"
+                                                        aria-pressed="false" data-original-title="">
+                                                <Form.Control type="radio" name="options" value="3" />
+                                                <div className="position-absolute center-absolute">
+                                                    <span className="font-weight-bold h4">UF 1,43</span>
+                                                    <small
+                                                        className="d-block text-black-50 font-weight-lighter">$41.010</small>
+                                                </div>
+                                            </Form.Label>
+                                        </Row>
+                                    )}
 
-                                    <div className="custom-control custom-radio sd-option-list mb-3">
-                                        <Form.Control type="radio" id="red2" name="eligered" className="custom-control-input" value="red2" onChange={this.props.handleChange}/>
-                                            <Form.Label className="custom-control-label text-col1 font-weight-bold h4" htmlFor="red2">
-                                                RED 2
-                                                <ul className="row h5 text-black-50 font-weight-lighter list-inputradio">
-                                                    <li>Clínica Santa María</li>
-                                                    <li>Clínica Indisa</li>
-                                                    <li>Red Salud Vitacura</li>
-                                                    <li>Christus Marcoleta</li>
-                                                </ul>
-                                            </Form.Label>
-                                    </div>
+                                </div>
 
-                                    <div className="custom-control custom-radio sd-option-list mb-3">
-                                        <Form.Control type="radio" id="red3" name="eligered" className="custom-control-input" value="red3" onChange={this.props.handleChange}/>
-                                            <Form.Label className="custom-control-label text-col1 font-weight-bold h4" htmlFor="red3">
-                                                RED 3
-                                                <ul className="row h5 text-black-50 font-weight-lighter list-inputradio">
-                                                    <li>Christus San Carlos</li>
-                                                    <li>Universidad de los Andes</li>
-                                                    <li>Meds</li>
-                                                </ul>
-                                            </Form.Label>
-                                    </div>
-
-                                    <div className="custom-control custom-radio sd-option-list mb-3">
-                                        <Form.Control type="radio" id="red4" name="eligered" className="custom-control-input" value="red4" onChange={this.props.handleChange}/>
-                                            <Form.Label className="custom-control-label text-col1 font-weight-bold h4" htmlFor="red4">
-                                                RED 4
-                                                <ul className="row h5 text-black-50 font-weight-lighter list-inputradio">
-                                                    <li>Clínica Alemana</li>
-                                                    <li>Clínica Las Condes</li>
-                                                </ul>
-                                            </Form.Label>
-                                    </div>
-                                </Form.Group>
-
-                                <Form.Group>
-                                    <h4 className="font-weight-bold text-col1">Elige tu deducible</h4>
-                                    <h5 className="text-secondary">Monto de copago acumulado que tienes que pagar por
-                                        persona antes de que se activen las coberturas. Mientras más alto sea el
-                                        deducible, más bajo será tu pago mensual.</h5>
-                                </Form.Group>
-                                <Form.Group className="mb-3">
-                                    <div className="custom-control custom-radio sd-option-list mb-3">
-                                        <Form.Control type="radio" id="deducible1" name="deducible" className="custom-control-input" value="10" onChange={this.props.handleChange}/>
-                                            <Form.Label className="custom-control-label text-col1 font-weight-bold h4" htmlFor="deducible1">
-                                                UF 10
-                                                <small className="row h5 text-black-50 font-weight-lighter pl-3">
-                                                    $287.000
-                                                </small>
-                                            </Form.Label>
-                                    </div>
-
-                                    <div className="custom-control custom-radio sd-option-list mb-3">
-                                        <Form.Control type="radio" id="deducible2" name="deducible" className="custom-control-input" value="15" onChange={this.props.handleChange}/>
-                                            <Form.Label className="custom-control-label text-col1 font-weight-bold h4" htmlFor="deducible2">
-                                                UF 15
-                                                <small className="row h5 text-black-50 font-weight-lighter pl-3">
-                                                    $430.500
-                                                </small>
-                                            </Form.Label>
-                                    </div>
-
-                                    <div className="custom-control custom-radio sd-option-list mb-3">
-                                        <Form.Control type="radio" id="deducible3" name="deducible" className="custom-control-input" value="20" onChange={this.props.handleChange}/>
-                                            <Form.Label className="custom-control-label text-col1 font-weight-bold h4" htmlFor="deducible3">
-                                                UF 20
-                                                <small className="row h5 text-black-50 font-weight-lighter pl-3">
-                                                    $574.000
-                                                </small>
-                                            </Form.Label>
-                                    </div>
-                                </Form.Group>
-                                <div className="form-group ml-5 mb-3 h6 text-black-50 font-italic">*Valor referencial en pesos.</div>
 
                                 <div className="form-group mt-5">
                                     <button className="btn bg-col1 text-white d-block mx-auto float-md-right my-2 py-3 px-4 shadow-lg mr-1 btn-bubble" onClick={this.props._next}>Siguiente</button>
                                     <button className="btn float-md-right text-col1 text-hv-col1 d-block mx-auto mr-md-2 my-2 py-3 px-4 btn-bubble" onClick={this.props._prev}>Volver</button>
                                 </div>
+
 
                             </Col>
                         </Row>
---
+++ client/src/utils/redes.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        name: "Red 1",
+        clinics: [
+            "Red Salud Stgo. y Providencia",
+            "Clínica Dávila",
+            "Clínica Vespucio"
+        ]
+    },
+    {
+        name: "Red 2",
+        clinics: [
+            "Clínica Santa María",
+            "Clínica Indisa",
+            "Red Salud Vitacura",
+            "Christus Marcoleta"
+        ]
+    },
+    {
+        name: "Red 3",
+        clinics: [
+            "Christus San Carlos",
+            "Universidad de los Andes",
+            "Meds"
+        ]
+    },
+    {
+        name: "Red 4",
+        clinics: [
+            "Clínica Alemana",
+            "Clínica Las Condes"
+        ]
+    }
+]
