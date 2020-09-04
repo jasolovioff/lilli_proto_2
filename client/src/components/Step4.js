209ae5d666b115f124837b36c5cf8619b0eaf34c
jorge <jorge@imatec.cl>
2020-09-04 12:11:52 -0400

    close detail box in last screen

--- client/src/components/Step4.js
+++ client/src/components/Step4.js
@@ -19,6 +19,7 @@ class Step4 extends Component{
             showDetalleCobro : false
         }
         this.toogleDetalleCobro = this.toogleDetalleCobro.bind(this);
+        this.hideDetalleCobro = this.hideDetalleCobro.bind(this);
     }
 
     toogleDetalleCobro = () => {
@@ -26,6 +27,12 @@ class Step4 extends Component{
         this.setState(prevState => ({
             showDetalleCobro : !prevState.showDetalleCobro
         }));
+    }
+
+    hideDetalleCobro(){
+        this.setState({
+            showDetalleCobro : false
+        })
     }
 
     getRedDetail(redTag){
@@ -210,7 +217,7 @@ class Step4 extends Component{
 
                                 <div className="form-group mt-5">
                                     <a href={"http://surveys-online.surveytaking.com/index.php/265219?lang=es&newtest=Y&owid=" + this.props.owid} className="btn bg-col1 text-white d-block mx-auto float-md-right my-2 py-3 px-4 shadow-lg mr-1 btn-bubble" onClick={this.props._next}>Continuar</a>
-                                    <span className="btn float-md-right text-col1 text-hv-col1 d-block mx-auto mr-md-2 my-2 py-3 px-4 btn-bubble" onClick={this.props._prev}>Volver</span>
+                                    <span className="btn float-md-right text-col1 text-hv-col1 d-block mx-auto mr-md-2 my-2 py-3 px-4 btn-bubble" onClick={(event)=> {this.props._prev(event); this.hideDetalleCobro()}}>Volver</span>
                                 </div>
 
                             </Col>
