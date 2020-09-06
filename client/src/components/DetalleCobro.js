f21f6ec7a9a4903dc076200794d38829ba5a5cde
jorge <jorge@imatec.cl>
2020-09-05 20:10:22 -0400

    Plan detail correction.

--- client/src/components/DetalleCobro.js
+++ client/src/components/DetalleCobro.js
@@ -16,15 +16,15 @@ class DetalleCobro extends Component{
         if (typeof (this.state.planDetails.cargas) != 'undefined' && this.state.planDetails.cargas.length > 0){
             return _.map(this.state.planDetails.cargas, carga => {
                 return (
-                    <Row fluid className="bg-light border-bottom py-1" key={"carga"+carga.age}>
-                        <Col xs={5} sm={4} className="text-left text-muted">Carga {carga.age} Años</Col>
+                    <Row className="row-fluid bg-light border-bottom py-1" key={"carga"+carga.age}>
+                        <Col xs={3} sm={4} className="text-left text-muted">Carga {carga.age} Años</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">{this.floatFormat.format(carga.basePrice)}</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">X</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">{this.floatFormat.format(carga.ageFactor)}</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">+</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">{this.floatFormat.format(this.state.planDetails.GES)}</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">=</Col>
-                        <Col xs={1} sm={1} className="text-center text-muted">{this.floatFormat.format(carga.finalPrice.toFixed(2))}</Col>
+                        <Col xs={1} sm={2} className="text-center text-muted">{this.floatFormat.format(carga.finalPrice.toFixed(2))}</Col>
                     </Row>
                 )
             })
@@ -35,7 +35,7 @@ class DetalleCobro extends Component{
         let floatFormat = new Intl.NumberFormat('es-CL');
         return(
             <React.Fragment>
-                    <Row fluid className="bg-light border-bottom py-1">
+                    <Row className="row-fluid bg-light border-bottom py-1">
                         <Col xs={3} sm={4} className="font-weight-bold text-left text-muted"></Col>
                         <Col xs={1} sm={1} className="font-weight-bold text-left text-muted">Precio Base</Col>
                         <Col xs={1} sm={1} className="text-right text-muted"></Col>
@@ -43,25 +43,25 @@ class DetalleCobro extends Component{
                         <Col xs={1} sm={1} className="text-right text-muted"></Col>
                         <Col xs={1} sm={1} className="font-weight-bold text-center text-muted">Valor <br/>GES</Col>
                         <Col xs={1} sm={1} className="text-right text-muted"></Col>
-                        <Col xs={1} sm={1} className="font-weight-bold text-left text-muted">Valor final</Col>
+                        <Col xs={1} sm={2} className="font-weight-bold text-left text-muted">Valor final</Col>
                     </Row>
-                    <Row fluid className="bg-light border-bottom py-1">
-                        <Col xs={5} sm={4} className="text-left text-muted">Titular</Col>
+                    <Row className="row-fluid bg-light border-bottom py-1">
+                        <Col xs={3} sm={4} className="text-left text-muted">Titular</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">{this.floatFormat.format(this.state.planDetails.cotizante.basePrice)}</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">X</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">{this.floatFormat.format(this.state.planDetails.cotizante.ageFactor)}</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">+</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">{this.floatFormat.format(this.state.planDetails.GES)}</Col>
                         <Col xs={1} sm={1} className="text-center text-muted">=</Col>
-                        <Col xs={1} sm={1} className="text-center text-muted">{this.floatFormat.format(this.state.planDetails.cotizante.finalPrice.toFixed(2))}</Col>
+                        <Col xs={1} sm={2} className="text-center text-muted">{this.floatFormat.format(this.state.planDetails.cotizante.finalPrice.toFixed(2))}</Col>
                     </Row>
                     {this.cargasDetail()}
-                    <Row fluid className="bg-light  pt-3">
+                    <Row className="row-fluid bg-light  pt-3">
                         <Col xs={12} className="text-right font-weight-bold pr-2">
                             Total: {floatFormat.format(this.props.totalPrice.toFixed(2))}
                         </Col>
                     </Row>
-                    <Row fluid className="bg-light py-3">
+                    <Row className="row-fluid bg-light py-3">
                         <Col xs={11}>
                             <h5 className="text-muted font-italic">
                                 *Todos los valores estan expresados en UF
